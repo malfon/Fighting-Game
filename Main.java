82a24d32e218a2d7931478b937d42ee121e7c058
malfon <cortneyjeans@gmail.com>
2014-09-25 21:23:50 -0400

    Update Main.java
    just the idle animation so far, but i've eliminated the flicker

--- Main.java
+++ Main.java
@@ -3,43 +3,47 @@ import java.nio.file.Paths;
 import org.jsfml.graphics.*;
 import org.jsfml.system.*;
 import org.jsfml.window.*;
+import org.jsfml.window.Keyboard.Key;
 import org.jsfml.window.event.*;
 
 public class Main
 {
+	
 	public static void main(String args[]) throws IOException, InterruptedException
 	{
 		int width=800, height=600;
 		RenderWindow window = new RenderWindow();
 		window.create(new VideoMode(width, height), "Fighting Game");
 		
-		Clock clock = new Clock();
-		Time dt = clock.restart();
+		window.setFramerateLimit(60);
+		
+		float frameRate=500, switchFrame=100, frameCounter=0;
+		Clock animTimer = new Clock();
+		Time animDelay;
+		int animSec;
+		
+		final int LEFT 	= 1;
+		final int RIGHT = 2;
+		
+		Vector2i pWalkVec = new Vector2i(0, 0);
 		
 		//Maps
 		
 		//UI
 		
 		//Fighters
-		boolean moving  = false;
-		
-		Texture p1t = new Texture();
-		p1t.loadFromFile(Paths.get("bin/p1.png"));
-		Sprite p1 = new Sprite();
-		p1.setTexture(p1t);
-		p1.setScale(.5f, .5f);
-		p1.setPosition(5, 390);
-		
-		Texture p1_idle1t = new Texture();
-		p1_idle1t.loadFromFile(Paths.get("bin/p1_idle1.png"));
-		Sprite p1_idle1 = new Sprite();
-		p1_idle1.setTexture(p1_idle1t);
-		p1_idle1.setScale(.5f, .5f);
-		p1_idle1.setPosition(p1.getPosition());
+		Texture ryuuTexture = new Texture();
+		ryuuTexture.loadFromFile(Paths.get("bin/ryuu.png"));
+		Sprite ryuuSprite = new Sprite();
+		ryuuSprite.setTexture(ryuuTexture);
+		ryuuSprite.setPosition(0, height-95);
 		
 		while(window.isOpen())
 		{
 			window.clear(Color.WHITE);
+			animDelay = animTimer.getElapsedTime();
+			animSec = (int) (animDelay.asSeconds());
+			
 			for(Event event : window.pollEvents())
 			{
 				switch(event.type)
@@ -51,23 +55,38 @@ public class Main
 					break;
 				}
 			}
-			if(!moving)
+			
+			frameCounter += frameRate * animTimer.restart().asSeconds()*3/2;
+			
+			if(frameCounter >= switchFrame && !Keyboard.isKeyPressed(Key.D) && !Keyboard.isKeyPressed(Key.A))
 			{
-				float count=0;
-				if(count>=0 && count<1)
+				frameCounter = 0;
+				if((pWalkVec.x*60) < ryuuTexture.getSize().x)
 				{
-					window.draw(p1);
+					ryuuSprite.setTextureRect(new IntRect(pWalkVec.x*60, pWalkVec.y*95, 60, 95));
+					pWalkVec = Vector2i.add(pWalkVec, new Vector2i(1, 0));
 				}
-				else if(count>=1 && count<2)
-				{
-					window.draw(p1_idle1);
-				}
-				if(count>2)
-					count=0;
 				else
-					count+=10;
-				System.out.println(count);
+				{
+					frameCounter = 0;
+					pWalkVec = new Vector2i(0, 0);
+				}
 			}
+			
+			if(Keyboard.isKeyPressed(Key.D))
+			{
+				pWalkVec = new Vector2i(pWalkVec.x, RIGHT);
+				ryuuSprite.move(1, 0);
+			}
+			
+			if(Keyboard.isKeyPressed(Key.A))
+			{
+				pWalkVec = new Vector2i(pWalkVec.x, LEFT);
+				ryuuSprite.move(-1, 0);
+			}
+			
+			window.draw(ryuuSprite);
+			
 			window.display();
 		}
 	}
